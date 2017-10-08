a07fea16ed176e1c7b7ce31cbe1a6ecc9d5d82d4
Kyle Owsen <kyle.owsen@gmail.com>
2017-10-08 00:16:52 -0700

    Added sample Monogame project

---
+++ Game1/MovementSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Minotaur;
+
+namespace Game1
+{
+    public class MovementSystem : GameSystem
+    {
+        public MovementSystem()
+        {
+            AddComponentConstraint<PositionComponent>();
+        }
+
+        public override void Update(TimeSpan time, Entity entity)
+        {
+            var position = entity.GetComponent<PositionComponent>();
+            position.X += 1;
+            position.Y += 1;
+        }
+    }
+}
---
+++ Game1/PositionComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Minotaur;
+
+namespace Game1
+{
+    public class PositionComponent : IComponent
+    {
+        public float X;
+        public float Y;
+    }
+}
